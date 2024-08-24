b6c55ea31550044a330a6f970833b7cfce0b7fcf
Yago <ylm@icomp.ufam.edu.br>
2024-08-24 18:37:59 -0400

    fix: atualização de importação de classe

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoriaController;
 use App\Http\Controllers\Api\ProdutoController;
+use App\Http\Controllers\Api\AuthController;
 
 
 // Rotas públicas
