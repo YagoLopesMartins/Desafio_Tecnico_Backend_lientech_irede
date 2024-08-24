357d615cc706823081e0740541464547e11003cd
Yago <ylm@icomp.ufam.edu.br>
2024-08-24 13:42:31 -0400

    feat: Criando request o qual garante regras de validacoes para  Categoria

---
+++ laravel-app/app/Http/Requests/CategoriaRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CategoriaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'erros' => $validator->errors(),
+        ], 422));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|max:100'
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'nome.required' => 'Campo nome da categoria é obrigatorio',
+            'nome.max' => 'O nome da categoria não pode ter mais que 100 caracteres.'
+        ];
+    }
+}
