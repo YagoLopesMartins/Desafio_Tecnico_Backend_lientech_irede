3433c50d220ae830f055acf6de1f64e17557c954
Yago <ylm@icomp.ufam.edu.br>
2024-08-24 23:29:31 -0400

    fix: rename classes de migration para o uso de ACL

--- laravel-app/database/migrations/2024_08_25_023222_create_role_user_table.php
+++ laravel-app/database/migrations/2024_08_25_023222_create_role_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateRoleUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
-            // $table->id();
             $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->primary(['role_id', 'user_id']);
-            // $table->timestamps();
         });
     }
 
